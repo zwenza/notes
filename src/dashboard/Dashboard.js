781d85bf530edcf75f30eb4cd935a98637bec24c
David Jöch <david.joech@catalysts.cc>
2018-03-10 15:12:16 +0100

    added scss

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Note from '../note/Note';
+import './Dashboard.scss';
 
 export default class Dashboard extends React.Component {
     render() {
         return (
-            <div>
+            <div className="test">
                 <h2>Dashboard!</h2>
+                <p>Test</p>
                 <Note />
             </div>
         );
