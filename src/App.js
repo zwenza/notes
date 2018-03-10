9ef05522a196459d2dba773a9493593846614583
David Jöch <david.joech@catalysts.cc>
2018-03-10 15:59:03 +0100

    added airbnb datepicker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Dashboard from './dashboard/Dashboard';
 import './App.scss';
 
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
 class App extends Component {
   render() {
     return (
--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import Note from '../note/Note';
+import { SingleDatePicker } from 'react-dates';
+import moment from 'moment';
 import './Dashboard.scss';
 
 export default class Dashboard extends React.Component {
+    state = {
+        datePickerActive: false,
+        selectedDate: undefined
+    }
+
     render() {
         return (
             <div className="container">
-                <h2>Calendar TODO</h2>
-                <Note />
+                <div className="header">
+                    <SingleDatePicker
+                        date={moment()}
+                        onDateChange={date => {
+                            this.setState(state => {
+                                console.log('date changed');
+
+                                state.datePickerActive = false;
+                                state.selectedDate = date;
+                            })
+                        }}
+                        focused={this.state.datePickerActive}
+                        onFocusChange={({ focused }) => { console.log('focus'); this.setState(state => state.datePickerActive = !state.datePickerActive) }}
+                    />
+                </div>
+                <div className="content">
+                    <div>
+                        <h2>Metadata</h2>
+                        <br />
+                        <p>Created 10.03.2018</p>
+                    </div>
+                    <Note />
+                </div>
             </div>
         );
     }
